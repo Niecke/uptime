8f87664f7e0d1d850477181e3306a7f50404fecf
Daniel Niecke <daniel@niecke-it.de>
2026-07-03 10:19:23 +0200

    fix possible nil pointer access

--- internal/alerting/slack.go
+++ internal/alerting/slack.go
@@ -11,12 +11,13 @@ func Notify(slack_api string, url string, url_status_code int, url_error string,
 	slog.Debug("Notify Slack channel", "slack_api", slack_api)
 	//TODO: use json.Marshal here; otherwise we can not add url_error here
 	body := fmt.Sprintf(`{"text":"%s is down for %d retries: %d"}`, url, threshold, url_status_code)
-	resp, err := http.Post(slack_api, "application/json", strings.NewReader((body)))
+	resp, err := http.Post(slack_api, "application/json", strings.NewReader(body))
 	if err != nil {
 		slog.Error("There was an error while sending slack notification", "error", err)
+		return
 	}
-	slog.Debug("reponse from slack", "status_code", resp.StatusCode, "resp", resp.Body)
-	slog.Info("Sent notify to slack", "url", url)
-
 	defer resp.Body.Close()
+
+	slog.Debug("response from slack", "status_code", resp.StatusCode, "resp", resp.Body)
+	slog.Info("Sent notify to slack", "url", url)
 }
